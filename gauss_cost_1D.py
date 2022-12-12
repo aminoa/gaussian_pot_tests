ae4839de80602c1d1c3188cfeb6b757adff62ac7
aminoa <28660350+aminoa@users.noreply.github.com>
2022-12-12 17:47:20 -0500

    refactored, added const matrix

--- gauss_cost_1D.py
+++ gauss_cost_1D.py
@@ -11,8 +11,13 @@ from ot.datasets import make_1D_gauss as gauss
 from sklearn.metrics import mean_squared_error
 import math
 import time
+from operator import add
 
 #TODO: implement constant cost matrix, importing alternate datasets for testing
+
+def generate_square_ones(size):
+    M = np.ones(shape=(size, size))
+    return M / np.sum(M)
 
 # generates random square matrix 
 def generate_square_random(size):
@@ -32,40 +37,52 @@ def generate_square_gaussian(size, fwhm = 3, center=None):
     M =  np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
     return M / np.sum(M)
 
-def benchmark(a, b, M, lambd, verbose=False):
+# runs a benchmark for a, b, M, lambd values 
+def benchmark(a, b, M, lambd, hornIterations=100000, verbose=False):
     # network simplex solver 
     start_time = time.time()
     G0 = ot.emd(a, b, M)
     end_time = time.time()
     G0_time = end_time - start_time 
-    # print(G0)
-    # print(G0_time)
 
-    # # sinkhorn/Greenkhorn solver (bregman)
+    # sinkhorn solver (bregman)
     start_time = time.time()
-    GS = ot.sinkhorn(a, b, M, lambd, numItermax=100000)
+    GS = ot.sinkhorn(a, b, M, lambd, numItermax=hornIterations)
     end_time = time.time()
-
-    GS_mse = mean_squared_error(GS, G0)
     GS_time = end_time - start_time
+    GS_mse = mean_squared_error(GS, G0)
 
+    # greenkhorn solver (bregman)
     start_time = time.time()
-    GG = ot.bregman.greenkhorn(a, b, M, lambd, numItermax=100000)
+    GG = ot.bregman.greenkhorn(a, b, M, lambd, numItermax=hornIterations)
     end_time = time.time()
-
     GG_mse = mean_squared_error(GG, G0)
     GG_time = end_time - start_time
 
     if verbose:
         print("GS_MSE: " + str(GS_mse))
         print("GG_MSE: " + str(GG_mse))
+        print("G0_Time: " + str(G0_time))
+        print("GS_Time: " + str(GS_time))
+        print("GG_Time: " + str(GG_time))
 
-    print("G0_Time: " + str(G0_time))
-    print("GS_Time: " + str(GS_time))
-    print("GG_Time: " + str(GG_time))
+    return [G0_time, GS_time, GG_time, GS_mse, GG_mse]
 
-    return G0_time, GS_time, GG_time, GS_mse, GG_mse, 
+def repeated_benchmark(a, b, M, lambd, iterations, horn_iterations=100000, verbose=False):
+    results = [0, 0, 0, 0, 0]
+    for i in range(iterations):
+        bench = benchmark(a, b, M, lambd, horn_iterations)
+        results = [sum(x) for x in zip(results, bench)]
 
+    if verbose:
+        print("G0_Time: " + str(results[0]))
+        print("GS_Time: " + str(results[1]))
+        print("GG_Time: " + str(results[2]))
+        print("GS_MSE: " + str(results[3]))
+        print("GG_MSE: " + str(results[4]))
+    
+    return [val / iterations for val in results]
+# marginals always randomly generated 
 def generate_marginals(size, max):
     a = np.random.randint(0, max, size=(size))
     b = np.random.randint(0, max, size=(size))
@@ -75,55 +92,25 @@ def generate_marginals(size, max):
     b = b / np.sum(b)
     return a, b
 
-results = [] 
+# results = [] 
 
 size = 1000
 max = 10000
-
 a, b = generate_marginals(size, max)
+
+lambd = 0.001
+iterations = 10
+horn_iterations = 1000000
+
+print("M_C Results")
+M_C = generate_square_ones(size)
+M_C_results = repeated_benchmark(a, b, M_C, lambd, iterations, horn_iterations, verbose=True)
+
+print("M_R Results")
 M_R = generate_square_random(size)
+M_R_results = repeated_benchmark(a, b, M_R, lambd, iterations, horn_iterations, verbose=True)
+
+print("M_G Results")
 M_G = generate_square_gaussian(size)
+M_G_results = repeated_benchmark(a, b, M_G, lambd, iterations, horn_iterations, verbose=True)
 
-M_R_results = [0,0,0,0,0]
-M_G_results = [0,0,0,0,0]
-
-iter = 100
-
-for i in range(iter): 
-    G0_time, GS_time, GG_time, GS_mse, GG_mse = benchmark(a, b, M_R, 0.001)
-    M_R_results[0] += G0_time
-    M_R_results[1] += GS_time
-    M_R_results[2] += GG_time 
-    M_R_results[3] += GS_mse
-    M_R_results[4] += GG_mse   
-
-    G0_time, GS_time, GG_time, GS_mse, GG_mse = benchmark(a, b, M_G, 0.001)
-    M_G_results[0] += G0_time
-    M_G_results[1] += GS_time
-    M_G_results[2] += GG_time 
-    M_G_results[3] += GS_mse
-    M_G_results[4] += GG_mse
-
-for i in range(len(M_R)):
-    M_R[i] /= iter
-    M_G[i] /= iter
-
-print("Iterations: " + str(iter))
-print("Size: " + str(size))
-print("Max Value for Array: " + str(max))
-
-print("Random G0_Time: " + str(M_R_results[0]))
-print("Random GS_Time: " + str(M_R_results[1]))
-print("Random GG_Time: " + str(M_R_results[2]))
-print("Random GS_MSE: " + str(M_R_results[3]))
-print("Random GG_MSE: " + str(M_R_results[4]))
-
-print("Gaussian G0_Time: " + str(M_G_results[0]))
-print("Gaussian GS_Time: " + str(M_G_results[1]))
-print("Gaussian GG_Time: " + str(M_G_results[2]))
-print("Gaussian GS_MSE: " + str(M_G_results[3]))
-print("Gaussian GG_MSE: " + str(M_G_results[4]))
-
-
-# print(M_R)
-# print(M_G)
